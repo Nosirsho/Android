3d8c426b0fd9f4cbcb0ac13db159f944fbd33d36
Kholmamadov N <Pamirikanec-m17@mail.ru>
2020-07-13 09:37:40 +0400

    add DrincActivity

--- Starbuzz/app/src/main/java/com/kholmamadov/starbuzz/DrinkCategoryActivity.java
+++ Starbuzz/app/src/main/java/com/kholmamadov/starbuzz/DrinkCategoryActivity.java
@@ -2,7 +2,10 @@ package com.kholmamadov.starbuzz;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,6 +21,16 @@ public class DrinkCategoryActivity extends Activity {
                 Drink.drinks);
         ListView listDrinks = (ListView)findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
-        
+        //Создание слушателя
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listDrinks, View itemView, int position, long id) {
+                //Передача категории выбранного из списка
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        };
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
